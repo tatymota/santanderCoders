6cbb33cbb9ca1468cb16f5c3a691e71f88883458
tatymota <tatyanna.mota@hotmail.com>
2022-10-21 21:46:31 -0300

    refactor: include "media" and try/catch

--- MainExercicio.java
+++ MainExercicio.java
@@ -2,55 +2,86 @@ import java.util.Scanner;
 
 public class MainExercicio {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite a quantidade de temperaturas a serem convertidas:");
-        double numeroDeConversoes = scanner.nextInt();
 
-        while(numeroDeConversoes > 0) {
-            System.out.println("Digite a unidade de temperatura de origem:");
-            String unidadeParaConverter = scanner.next();
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite a quantidade de temperaturas a serem convertidas:");
+            double numeroDeConversoes = scanner.nextInt();
 
-            System.out.println("Digite a unidade de temperatura de destino:");
-            String unidadeDeDestino = scanner.next();
+            double temperaturasDeOrigem = 0;
+            double temperaturasDeDestino = 0;
+            double numeroDeTemperaturas = 0;
 
-            System.out.println("Digite a temperatura de origem:");
-            double temperatura = scanner.nextDouble();
+            while(numeroDeConversoes > 0) {
+                System.out.println("Digite a unidade de temperatura de origem:");
+                String unidadeParaConverter = scanner.next();
 
-            if("C".equals(unidadeParaConverter)) {
-                if("K".equals(unidadeDeDestino)) {
-                    double temperaturaConvertida = temperatura + 273.15;
-                    System.out.println("Conversao: " + temperatura + " Celsius = " + temperaturaConvertida + " Kelvin");
-                } else if("F".equals(unidadeDeDestino)){
-                    double temperaturaConvertida = ((temperatura * 9) / 5) + 32;
-                    System.out.println("Conversao: " + temperatura + " Celsius = " + temperaturaConvertida + " Fahrenheit");
+                System.out.println("Digite a unidade de temperatura de destino:");
+                String unidadeDeDestino = scanner.next();
+
+                System.out.println("Digite a temperatura de origem:");
+                double temperatura = scanner.nextDouble();
+
+                if("C".equals(unidadeParaConverter)) {
+                    if("K".equals(unidadeDeDestino)) {
+                        double temperaturaConvertida = temperatura + 273.15;
+                        System.out.println("Conversao: " + temperatura + " Celsius = " + temperaturaConvertida + " Kelvin");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else if("F".equals(unidadeDeDestino)){
+                        double temperaturaConvertida = ((temperatura * 9) / 5) + 32;
+                        System.out.println("Conversao: " + temperatura + " Celsius = " + temperaturaConvertida + " Fahrenheit");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else {
+                        System.out.println("Unidade de destino invalida");
+                    }
+                } else if("K".equals(unidadeParaConverter)){
+                    if("C".equals(unidadeDeDestino)) {
+                        double temperaturaConvertida = temperatura - 273.15;
+                        System.out.println("Conversao: " + temperatura + " Kelvin = " + temperaturaConvertida + " Celsius");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else if("F".equals(unidadeDeDestino)){
+                        double temperaturaConvertida = (temperatura - 273.15) * 9.0/5.0 + 32;
+                        System.out.println("Conversao: " + temperatura + " Kelvin = " + temperaturaConvertida + " Fahrenheit");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else {
+                        System.out.println("Unidade de destino invalida");
+                    }
+                } else if("F".equals(unidadeParaConverter)){
+                    if("C".equals(unidadeDeDestino)) {
+                        double temperaturaConvertida = (temperatura -32) * 5/9;
+                        System.out.println("Conversao: " + temperatura + " Fahrenheit = " + temperaturaConvertida + " Celsius");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else if("K".equals(unidadeDeDestino)){
+                        double temperaturaConvertida = ((temperatura -32) * 5/9) + 273.15;
+                        System.out.println("Conversao: " + temperatura + " Fahrenheit = " + temperaturaConvertida + " Kelvin");
+                        temperaturasDeOrigem += temperatura;
+                        temperaturasDeDestino += temperaturaConvertida;
+                        numeroDeTemperaturas++;
+                    } else {
+                        System.out.println("Unidade de destino invalida");
+                    }
                 } else {
-                    System.out.println("Unidade de destino invalida");
+                    System.out.println("Unidade de origem invalida");
                 }
-            } else if("K".equals(unidadeParaConverter)){
-                if("C".equals(unidadeDeDestino)) {
-                    double temperaturaConvertida = temperatura - 273.15;
-                    System.out.println("Conversao: " + temperatura + " Kelvin = " + temperaturaConvertida + " Celsius");
-                } else if("F".equals(unidadeDeDestino)){
-                    double temperaturaConvertida = (temperatura - 273.15) * 9.0/5.0 + 32;
-                    System.out.println("Conversao: " + temperatura + " Kelvin = " + temperaturaConvertida + " Fahrenheit");
-                } else {
-                    System.out.println("Unidade de destino invalida");
-                }
-            } else if("F".equals(unidadeParaConverter)){
-                if("C".equals(unidadeDeDestino)) {
-                    double temperaturaConvertida = (temperatura -32) * 5/9;
-                    System.out.println("Conversao: " + temperatura + " Fahrenheit = " + temperaturaConvertida + " Celsius");
-                } else if("K".equals(unidadeDeDestino)){
-                    double temperaturaConvertida = ((temperatura -32) * 5/9) + 273.15;
-                    System.out.println("Conversao: " + temperatura + " Fahrenheit = " + temperaturaConvertida + " Kelvin");
-                } else {
-                    System.out.println("Unidade de destino invalida");
-                }
-            } else {
-                System.out.println("Unidade de origem invalida");
+
+                numeroDeConversoes = numeroDeConversoes - 1;
             }
+            System.out.println("Media das temperaturas de origem: " + (temperaturasDeOrigem/numeroDeTemperaturas));
+            System.out.println("Media das temperaturas de destino: " + (temperaturasDeDestino/numeroDeTemperaturas));
 
-            numeroDeConversoes = numeroDeConversoes - 1;
+        }catch(Exception e) {
+            System.out.println("Ocorreu um erro nos valores digitados");
         }
+
     }
 }
